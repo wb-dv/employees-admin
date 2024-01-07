0496dd1b8799695c7b9ddc7f95a5104aee60adbb
wb-dv <isss.300@mail.ru>
2024-01-07 17:32:06 +0300

    setup prisma service and swagger

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Employees Admin')
+    .setDescription('The Employees Admin API description')
+    .setVersion('0.1')
+    .addServer('/api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
