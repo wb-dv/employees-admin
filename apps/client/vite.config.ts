c64d893256d9e89bc7edb6e67767570b403174b0
wb-dv <isss.300@mail.ru>
2024-05-27 21:39:44 +0300

    8, feat: restruct src folder, init tailwind and setup aliases

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
 });
