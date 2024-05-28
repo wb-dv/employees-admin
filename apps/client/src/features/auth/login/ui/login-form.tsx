9b3a5dd167d8cc52a629b00f85fc5e9120694f5f
wb-dv <isss.300@mail.ru>
2024-05-28 23:20:52 +0300

    9, feat: create login feature and login form

---
+++ apps/client/src/features/auth/login/ui/login-form.tsx
@@ -0,0 +1,68 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@shared/ui";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@shared/ui/form";
+import { Input } from "@shared/ui/input";
+
+import { LoginSchema, defaultLoginValues, loginSchema } from "../model";
+
+export const LoginForm = () => {
+  const form = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: defaultLoginValues,
+  });
+
+  const onSubmit = (data: LoginSchema) => {
+    console.log("login values: ", data);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex w-full flex-col gap-6"
+      >
+        <FormField
+          control={form.control}
+          name={"email"}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Enter email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name={"password"}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Enter password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Login</Button>
+      </form>
+    </Form>
+  );
+};
