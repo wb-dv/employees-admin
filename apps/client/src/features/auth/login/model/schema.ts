7e4f89441af58aea753b5d2478e6aa75bc8c67de
wb-dv <isss.300@mail.ru>
2024-05-31 02:39:29 +0300

    10, refactor: reuse credentials schema

--- apps/client/src/features/auth/login/model/schema.ts
+++ apps/client/src/features/auth/login/model/schema.ts
@@ -1,15 +1,10 @@
 import * as z from 'zod';
 
+import { credentialsSchema } from '@features/auth/model';
+
 import { authControllerLoginBody } from '@shared/api';
 
-export const loginSchema = authControllerLoginBody.extend({
-  email: z
-    .string({ required_error: '"email" is required' })
-    .email({ message: 'Invalid email' }),
-  password: z
-    .string({ required_error: '"password" is required' })
-    .min(3, { message: 'Password must be at least 3 characters' }),
-});
+export const loginSchema = authControllerLoginBody.merge(credentialsSchema);
 
 export type LoginSchema = z.infer<typeof loginSchema>;
 
