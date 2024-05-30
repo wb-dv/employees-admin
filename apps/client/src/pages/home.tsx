493cd797395ab01157bcf360a4c46c01780ddcd7
wb-dv <isss.300@mail.ru>
2024-05-30 08:55:15 +0300

    9, fix,refactor: deploy shell script rename to app and fix start it from pacakge json

--- apps/client/src/pages/home.tsx
+++ apps/client/src/pages/home.tsx
@@ -1,10 +1,7 @@
-import { PageLoader } from '@shared/ui/loader';
-
 export const Home = () => {
   return (
-    // <div>
-    //   <h1>Home</h1>
-    // </div>
-    <PageLoader />
+    <div>
+      <h1>Home</h1>
+    </div>
   );
 };
